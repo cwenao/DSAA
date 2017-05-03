f93c2d2b32c152399ebbf271c321878739f9b51a
cwenao <chenwenhao@shiruxu.com>
2017-05-03 08:47:27 +0800

    init

---
+++ data-structure/src/main/java/com/cwenao/datastructure/ConflationSort.java
@@ -0,0 +1,93 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author cwenao
+ * @version $Id ConflationSort.java, v 0.1 2017-05-01 16:16 cwenao Exp $$
+ */
+public class ConflationSort {
+
+    /**
+     * 合并两个相邻的结果
+     * @param sortArray
+     * @param low
+     * @param mid
+     * @param high
+     */
+    public static void merger(int[] sortArray, int low, int mid, int high) {
+
+        int[] tmp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int x = 0;
+
+        while (i <= mid && j <= high) {
+            if (sortArray[i] <= sortArray[j]) {
+                tmp[x++] = sortArray[i++];
+            } else {
+                tmp[x++] = sortArray[j++];
+            }
+        }
+
+        while (i <= mid) {
+            tmp[x++] = sortArray[i++];
+        }
+
+        while (j <= high) {
+            tmp[x++] = sortArray[j++];
+        }
+
+        for(int k =0 ;k <tmp.length;k++) {
+            sortArray[low + k] = tmp[k];
+        }
+    }
+
+    //The recursion call the function
+    //n lgn
+
+    /**
+     * 分治法算法：
+     * 归并排序：递归与合并
+     * 分解、解决、合并
+     * 时间复杂度为 n(lgn)
+     * @param integers
+     * @param low
+     * @param high
+     * @return
+     */
+    public static int[] ConflationSortASC(int[] integers, int low, int high) {
+
+        if (null == integers || integers.length < 1) {
+            return null;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (low < high) {
+            ConflationSortASC(integers, low, mid);
+            ConflationSortASC(integers, mid + 1, high);
+            merger(integers, low, mid, high);
+        }
+
+        return integers;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] sorti = new int[]{1, 35, 2, 44, 100, 55};
+
+        int[] x = ConflationSortASC(sorti, 0, sorti.length - 1);
+        System.out.println(Arrays.toString(x));
+
+    }
+
+
+}
+
---
+++ data-structure/src/main/java/com/cwenao/datastructure/InsertionSort.java
@@ -0,0 +1,76 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure;
+
+/**
+ * Insertion Sort
+ * @author cwenao
+ * @version $Id InsertionSort.java, v 0.1 2017-05-01 14:41 cwenao Exp $$
+ */
+public class InsertionSort {
+
+    /**
+     * 插入排序
+     * 时间复杂度 n²
+     * @param integerArray
+     * @return
+     */
+    public static Integer[] insertionSortASC(Integer[] integerArray) {
+
+        if (null == integerArray || integerArray.length < 1) {
+            return null;
+        }
+        //取顶部元素开始，即1...j个元素都是已经排序
+        //满足 循环不变式：初始化、保持、终止
+        for (int j=1 ;j < integerArray.length;j++) {
+
+            int key = integerArray[j];
+            int i = j - 1;
+            //将当前元素依次
+            while (i > 0 && integerArray[i] > key) {
+                integerArray[i + 1] = integerArray[i];
+                i = i - 1;
+            }
+
+            integerArray[i+1] = key;
+
+        }
+
+        return integerArray;
+    }
+
+    public static Integer[] insertionSortDESC(Integer[] integerArray) {
+        if (null == integerArray || integerArray.length < 1) {
+            return null;
+        }
+
+        for(int j=1;j<integerArray.length;j++) {
+            int key = integerArray[j];
+            int i = j;
+
+            while (i >0   && key > integerArray[i-1]) {
+                integerArray[i] = integerArray[i-1];
+                i = i - 1;
+            }
+
+            integerArray[i] = key;
+        }
+        return integerArray;
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] sorti = new Integer[]{1, 35, 2, 44, 100, 55};
+        //Integer[] x = InsertionSort.insertionSortASC(sorti);
+        Integer[] x = InsertionSort.insertionSortDESC(sorti);
+        for (int ss : x) {
+            System.out.println(ss);
+        }
+
+    }
+
+
+}
+
