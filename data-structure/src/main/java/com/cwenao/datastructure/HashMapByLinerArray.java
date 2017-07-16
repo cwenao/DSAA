1dfb7035181b6806cc1d665a539077b02258f97b
cwenao <cwenao@126.com>
2017-07-16 15:09:44 +0800

    implements hashMap with linerarray

--- data-structure/src/main/java/com/cwenao/datastructure/HashMapByLinerArray.java
+++ data-structure/src/main/java/com/cwenao/datastructure/HashMapByLinerArray.java
@@ -48,7 +48,9 @@ public class HashMapByLinerArray<K,V> {
     }
 
     private int hash(K key) {
-        return (key.hashCode() & 0x7FFFFFF) % mapLen;
+        //0x7FFFFFFF 转换二进制为 0111 1111 1111 1111 1111 1111 1111 1111
+        //hashcode 可能为负数，求 & 后则为正数，此处作为index所以需要正数
+        return (key.hashCode() & 0x7FFFFFFF) % mapLen;
     }
 
     public V get(K key) {
@@ -58,6 +60,102 @@ public class HashMapByLinerArray<K,V> {
             }
         }
         return null;
+    }
+
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new NullPointerException("The key for map do not null");
+        }
+
+        if (currentKeyLen * 2 >= mapLen) {
+            extendSize(mapLen * 2);
+        }
+
+        int i;
+        for(i = hash(key); keys[i] !=null; i = (i+1) % mapLen) {
+            if (keys[i].equals(key)) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        keys[i] = key;
+        values[i] = value;
+        currentKeyLen++;
+
+    }
+
+    public void remove(K key) {
+        if (!contains(key)) {
+            return;
+        }
+
+        int index = hash(key);
+
+        while (!key.equals(keys[index])) {
+            index = (index + 1) % mapLen;
+        }
+
+        keys[index] = null;
+        values[index] = null;
+
+        index = (index + 1) % mapLen;
+
+        while (keys[index] != null) {
+            K ktmp = keys[index];
+            V vtmp = values[index];
+
+            keys[index] = null;
+            values[index] = null;
+
+            currentKeyLen--;
+            put(ktmp, vtmp);
+
+            index = (index + 1) % mapLen;
+        }
+
+        currentKeyLen--;
+
+        if (currentKeyLen * 2 > mapLen) {
+            extendSize(mapLen * 2);
+        }
+    }
+
+    private void extendSize(int extendLen) {
+        HashMapByLinerArray<K, V> tmp = new HashMapByLinerArray<>(extendLen);
+
+        for(int i =0 ;i < mapLen;i++) {
+            if (keys[i] != null) {
+                tmp.put(keys[i], values[i]);
+            }
+        }
+
+        keys = tmp.keys;
+        values = tmp.values;
+        mapLen = tmp.mapLen;
+
+    }
+
+    public static void main(String[] args) {
+        HashMapByLinerArray<String, String> hashMapByLinerArray = new HashMapByLinerArray<>(10);
+        hashMapByLinerArray.put("a", "A");
+        hashMapByLinerArray.put("b", "B");
+        hashMapByLinerArray.put("c", "C");
+        hashMapByLinerArray.put("d", "D");
+        hashMapByLinerArray.put("e", "E");
+        hashMapByLinerArray.put("F", "F");
+
+        System.out.println(hashMapByLinerArray.size());
+
+        System.out.println(hashMapByLinerArray.contains("a"));
+
+        System.out.println(hashMapByLinerArray.mapLen);
+        System.out.println(hashMapByLinerArray.currentKeyLen);
+        hashMapByLinerArray.remove("a");
+        System.out.println(hashMapByLinerArray.get("b"));
+        System.out.println(hashMapByLinerArray.contains("a"));
+
+
     }
 
 }
