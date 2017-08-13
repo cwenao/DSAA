3d6902a4d5e9e01307980151200dc5110d997be1
cwenao <cwenao@126.com>
2017-08-14 06:00:29 +0800

    add the heap sort

---
+++ data-structure/src/main/java/com/cwenao/datastructure/graph/DepthFirstSearchByAdjacentList.java
@@ -0,0 +1,85 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure.graph;
+
+/**
+ * @author cwenao
+ * @version $Id DepthFirstSearchByAdjacentList.java, v 0.1 2017-08-13 08:43 cwenao Exp $$
+ */
+public class DepthFirstSearchByAdjacentList<T extends Comparable> {
+
+    private boolean[] marked; //标识已经走过的路径
+
+    private int count; //标识连接数量
+
+    private Graph<Integer> graph; //输入图
+
+    private int data; //需要查找的顶点
+
+    public DepthFirstSearchByAdjacentList(Graph<Integer> graph,int data) {
+
+        marked = new boolean[graph.getV()];
+        checkedVertexs(data);
+        this.graph = graph;
+        this.data = data;
+    }
+
+
+    public void dfs() {
+        dfs(graph, data);
+    }
+
+    /**
+     * recursion the dfs
+     * @param graph
+     * @param v
+     */
+    private void dfs(Graph<Integer> graph, int v) {
+        count++;
+        marked[v] = true;
+        for (int w : graph.adj(v)) {
+            if (!isMarked(w)) {
+                dfs(graph, w);
+            }
+        }
+    }
+
+    public boolean isMarked(int v) {
+        checkedVertexs(v);
+        return marked[v];
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    private void checkedVertexs(int v) {
+        if (v < 0 || v > marked.length) {
+            throw new IllegalArgumentException("vertex : " + v + "is not between 0 and " + (marked.length - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph<Integer> graph = new Graph<>(7);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 5);
+        graph.addEdge(5, 6);
+
+        DepthFirstSearchByAdjacentList dfs = new DepthFirstSearchByAdjacentList(graph, 0);
+
+        dfs.dfs();
+
+        for(int v = 0;v<graph.getV();v++) {
+            if (dfs.isMarked(v)) {
+                System.out.println("the vertex is : " +v);
+            }
+        }
+
+    }
+
+}
---
+++ data-structure/src/main/java/com/cwenao/datastructure/sort/HeapSort.java
@@ -0,0 +1,62 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure.sort;
+
+/**
+ * @author cwenao
+ * @version $Id HeapSort.java, v 0.1 2017-08-13 16:12 cwenao Exp $$
+ */
+public class HeapSort {
+
+
+    public void heapSortAES(int[] array) {
+        int len = array.length;
+
+        for (int i = len / 2-1; i > 0; i--) {
+            heapSink(array, i, len);
+        }
+
+        for (int i = len - 1; i > 0; i--) {
+            exch(array, 1, i);
+            heapSink(array, 1, i - 1);
+        }
+
+    }
+
+    private void heapSink(int[] array, int k, int len) {
+
+        while (2 * k <= len) {
+            int j = 2 * k;
+            if (j < len && !less(array, j, j + 1)) {
+                j++;
+            }
+            if (less(array, k, j)) {
+                break;
+            }
+            exch(array, k, j);
+            k = j;
+        }
+    }
+
+    private boolean less(int[] array, int source, int target) {
+        return array[source] < array[target];
+    }
+
+    private void exch(int[] array, int source, int target) {
+        int tmp = array[source];
+        array[source] = array[target];
+        array[target] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int array[] = {0,25,30,95,41,72,130,71,10,110,52,80};
+        HeapSort heapSort = new HeapSort();
+        heapSort.heapSortAES(array);
+        for (int x : array) {
+            System.out.println("this is the elements: " + x);
+        }
+    }
+
+}
