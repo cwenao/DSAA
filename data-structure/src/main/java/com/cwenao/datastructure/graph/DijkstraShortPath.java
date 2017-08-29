ee4e05544802121122329ba6ae3b8a85bc0b8788
cwenao <cwenao@126.com>
2017-08-29 23:18:12 +0800

    a

--- data-structure/src/main/java/com/cwenao/datastructure/graph/DijkstraShortPath.java
+++ data-structure/src/main/java/com/cwenao/datastructure/graph/DijkstraShortPath.java
@@ -13,7 +13,8 @@ import java.util.Map;
  * （2）从Y中找出距离起点最近的节点，放入U中，并更新与这个节点有边直接相连的相邻节点到起始节点的最短距离。
  * （3）重复步骤（2）直到Y集合为空，即从起点出发可以到达的所有顶点都在集合U中为止。
  *
- * @author cwenao
+ * @author cwenaossvn
+ *
  * @version $Id DijkstraShortPath.java, v 0.1 2017-08-22 23:24 cwenao Exp $$
  */
 public class DijkstraShortPath<T extends Comparable> {
@@ -23,7 +24,6 @@ public class DijkstraShortPath<T extends Comparable> {
     private Map<String, Integer> vertexes;
 
     private DirectedEdge[] directedEdge;
-
 
 
 
