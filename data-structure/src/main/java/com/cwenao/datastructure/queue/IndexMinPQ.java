0ed58ac724145b56dada2408efae02b7f2110e11
cwenao <cwenao@126.com>
2017-09-02 23:34:02 +0800

    a

---
+++ data-structure/src/main/java/com/cwenao/datastructure/queue/IndexMinPQ.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure.queue;
+
+/**
+ * @author cwenao
+ * @version $Id IndexMinPQ.java, v 0.1 2017-09-02 23:33 cwenao Exp $$
+ */
+public class IndexMinPQ {
+}
