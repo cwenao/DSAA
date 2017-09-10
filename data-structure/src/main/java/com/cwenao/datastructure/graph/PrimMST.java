2432580d08c86222ee02673ec4bc0238336e1fe3
cwenao <cwenao@126.com>
2017-09-10 15:26:34 +0800

    add the prim and comment

---
+++ data-structure/src/main/java/com/cwenao/datastructure/graph/PrimMST.java
@@ -0,0 +1,18 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure.graph;
+
+/**
+ * 图G，顶点集合为U，设集合空集合V，边集合UV，
+ * 1.从U中选取顶点a为起始点，将a加入V，将a从U中删除
+ * 2.以a为起点在集合UV中找到一点b使得a到b的权值最小
+ * 3.将b加入V，将b从U中删除
+ * 4.重复2、3直到U为空
+ * @author cwenao
+ * @version $Id PrimMST.java, v 0.1 2017-09-10 17:36 cwenao Exp $$
+ */
+public class PrimMST {
+
+}
