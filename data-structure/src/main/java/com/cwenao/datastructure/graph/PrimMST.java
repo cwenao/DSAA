61814e28dd3e59baa7f9fd3c010b104faf0298a6
cwenao <cwenao@126.com>
2017-09-13 17:49:02 +0800

    update the primMST

--- data-structure/src/main/java/com/cwenao/datastructure/graph/PrimMST.java
+++ data-structure/src/main/java/com/cwenao/datastructure/graph/PrimMST.java
@@ -6,6 +6,9 @@ package com.cwenao.datastructure.graph;
 
 import com.cwenao.datastructure.queue.IndexMinPQ;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * 图G，顶点集合为U，设集合空集合V，边集合UV，
  * 1.从U中选取顶点a为起始点，将a加入V，将a从U中删除
@@ -22,8 +25,61 @@ public class PrimMST {
     private IndexMinPQ<Double> pq;
 
 
-    private void scan(EdgeWeightGraph edgeWeightGraph) {
+    public PrimMST(EdgeWeightGraph graph) {
+        edgeTo = new Edge[graph.getV()];
+        distTo = new double[graph.getV()];
+        marked = new boolean[graph.getV()];
 
+        pq = new IndexMinPQ<>(graph.getV());
+
+        for (int v = 0; v < graph.getV(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        for (int v = 0; v < graph.getV(); v++) {
+            if (!marked[v]) {
+                scan(graph, v);
+            }
+        }
+    }
+
+    private void prim(EdgeWeightGraph graph, int s) {
+        distTo[s] = 0.0;
+        pq.insert(s, distTo[s]);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            scan(graph, v);
+        }
+    }
+
+    private void scan(EdgeWeightGraph edgeWeightGraph, int v) {
+        marked[v] = true;
+        for (Edge edge : edgeWeightGraph.adj(v)) {
+            int w = edge.getOther(v);
+            if (marked[w]) {
+                continue;
+            }
+            if (edge.getWeight() < distTo[w]) {
+                distTo[w] = edge.getWeight();
+                edgeTo[w] = edge;
+                if (pq.contains(w)) {
+                    pq.decreaseKey(w, distTo[w]);
+                } else {
+                    pq.insert(w, distTo[w]);
+                }
+            }
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        Queue<Edge> edges = new ArrayDeque<>();
+
+        for (int v = 0; v < edgeTo.length; v++) {
+            Edge edge = edgeTo[v];
+            if (edge!=null) {
+                edges.offer(edge);
+            }
+        }
+        return edges;
     }
 
 }
