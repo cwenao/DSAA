e1b5c2cdca413423ebbe795d015856bb74c7a4d1
cwenao <cwenao@126.com>
2017-09-16 20:02:31 +0800

    add the topological

---
+++ data-structure/src/main/java/com/cwenao/datastructure/graph/Topological.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.datastructure.graph;
+
+/**
+ * @author cwenao
+ * @version $Id Topological.java, v 0.1 2017-09-16 20:01 cwenao Exp $$
+ */
+public class Topological {
+}
