34bb2912bfc6366e6f44db67dca8cf6075a61777
cwenao <cwenao@126.com>
2017-09-17 22:09:29 +0800

    add the comment to topological

--- data-structure/src/main/java/com/cwenao/datastructure/graph/Topological.java
+++ data-structure/src/main/java/com/cwenao/datastructure/graph/Topological.java
@@ -5,6 +5,11 @@
 package com.cwenao.datastructure.graph;
 
 /**
+ * 拓扑排序为DAG所有顶点的线性排序
+ * 1. 选择 DAG中入度为0（没有前驱）的顶点A，输出顶点A
+ * 2. 删除以顶点A为起点的边
+ * 3. 重复1、2步直到DAG顶点集合为空或者DAG中不存在入度为0的顶点
+ *
  * @author cwenao
  * @version $Id Topological.java, v 0.1 2017-09-16 20:01 cwenao Exp $$
  */
