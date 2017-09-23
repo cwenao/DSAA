d2ac2fc19013aa5e6537939fbb902ed6653c50a9
cwenao <cwenao@126.com>
2017-09-23 13:40:28 +0800

    add the GFG

---
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/GFG.java
@@ -0,0 +1,21 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.stackandqueue;
+
+/**
+ * @author cwenao
+ * @version $Id GFG.java, v 0.1 2017-09-23 13:31 cwenao Exp $$
+ */
+public class GFG {
+    private class DLLNode {
+        DLLNode preNode;
+        DLLNode next;
+        int data;
+
+        DLLNode(int data) {
+            this.data = data;
+        }
+    }
+}
