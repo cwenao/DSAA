0f7eb5672611d11ac9f8a96f5a5f1bcabe5c7855
cwenao <cwenao@126.com>
2017-09-25 14:58:12 +0800

    implement by queue

---
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/StackByQueue.java
@@ -0,0 +1,14 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.stackandqueue;
+
+/**
+ * implement by Queue queue = new LinkedList();
+ * @author cwenao
+ * @version $Id StackByQueue.java, v 0.1 2017-09-24 14:56 cwenao Exp $$
+ */
+public class StackByQueue {
+
+}
