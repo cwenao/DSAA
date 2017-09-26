8f1cc10d568eecdc6b65dc6124843270c5d1ca2c
cwenao <cwenao@126.com>
2017-09-26 15:04:14 +0800

    updte the stack by queue

--- data-structure/src/main/java/com/cwenao/trying/stackandqueue/StackByQueue.java
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/StackByQueue.java
@@ -4,6 +4,9 @@
  */
 package com.cwenao.trying.stackandqueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * implement by Queue queue = new LinkedList();
  * @author cwenao
@@ -11,4 +14,35 @@ package com.cwenao.trying.stackandqueue;
  */
 public class StackByQueue {
 
+    private Queue<Integer> queue = new LinkedList<>();
+
+    public void push(int x) {
+        queue.add(x);
+
+        for (int i = 0; i < queue.size(); i++) {
+            int y = queue.remove();
+            queue.add(y);
+        }
+    }
+
+    public int pop() {
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        int x = queue.remove();
+        return x;
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public static void main(String[] args) {
+        StackByQueue stackByQueue = new StackByQueue();
+        stackByQueue.push(1);
+        stackByQueue.push(30);
+        stackByQueue.push(25);
+        System.out.println(stackByQueue.top());
+        System.out.println(stackByQueue.pop());
+    }
 }
