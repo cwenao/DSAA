9eb6cf24318ab6373e378082c5c2501046f84ded
cwenao <cwenao@126.com>
2017-09-28 16:34:29 +0800

    update the Infix2Postfix

--- data-structure/src/main/java/com/cwenao/trying/stackandqueue/Infix2PostfixByStack.java
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/Infix2PostfixByStack.java
@@ -4,6 +4,8 @@
  */
 package com.cwenao.trying.stackandqueue;
 
+import java.util.Stack;
+
 /**
  * @author cwenao
  * @version $Id Infix2PostfixByStack.java, v 0.1 2017-09-27 16:06 cwenao Exp $$
@@ -24,5 +26,40 @@ public class Infix2PostfixByStack {
         }
     }
 
+    public static String infix2Postfix(String exp) {
+        StringBuffer sb = new StringBuffer();
+        Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sb.append(ch);
+            } else if ('(' == ch) {
+                stack.push(ch);
+            } else if (')' == ch) {
+                while (!stack.isEmpty() && '(' != stack.peek()) {
+                    sb.append(stack.pop());
+                }
+                if (!stack.isEmpty() && '(' != stack.peek()) {
+                    return "Invalid exp";
+                } else {
+                    stack.pop();
+                }
+            } else {
+                while (!stack.isEmpty() && prec(ch) <= prec(stack.peek())) {
+                    sb.append(stack.pop());
+                }
+                stack.push(ch);
+            }
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String exp = "a+b*(c^d-e)^(f+g*h)-i";
+        System.out.println(infix2Postfix(exp));
+    }
 }
