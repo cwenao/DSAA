7b4d4649c3318295c7b09dbdce784548c803bfaa
cwenao <cwenao@126.com>
2017-09-30 23:17:49 +0800

    update the stock span by stack

--- data-structure/src/main/java/com/cwenao/trying/stackandqueue/StockSpanByStack.java
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/StockSpanByStack.java
@@ -4,9 +4,37 @@
  */
 package com.cwenao.trying.stackandqueue;
 
+import java.util.Stack;
+
 /**
  * @author cwenao
  * @version $Id StockSpanByStack.java, v 0.1 2017-09-29 12:50 cwenao Exp $$
  */
 public class StockSpanByStack {
+    static void calculateSpan(int[] price, int[] result, int n) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && price[stack.peek()] <= price[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] price = {10, 4, 5, 90, 120, 80};
+        int n = price.length;
+        int[] result = new int[n];
+
+        calculateSpan(price, result, n);
+
+        for (int x : result) {
+            System.out.println(x);
+        }
+    }
 }
