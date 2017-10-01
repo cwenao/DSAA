9aa8fdb9f95d160591a940bda1e7a3efdfb7220b
cwenao <cwenao@126.com>
2017-10-01 08:51:21 +0800

    add the NGE

---
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/NextGreaterElementByStack.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.stackandqueue;
+
+/**
+ * @author cwenao
+ * @version $Id NextGreaterElementByStack.java, v 0.1 2017-10-01 08:50 cwenao Exp $$
+ */
+public class NextGreaterElementByStack {
+}
