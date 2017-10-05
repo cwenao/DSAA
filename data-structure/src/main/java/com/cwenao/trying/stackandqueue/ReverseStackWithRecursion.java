49f6eb5acf67d1e89d59a990cc2f5b515e784f4a
cwenao <cwenao@126.com>
2017-10-05 22:47:26 +0800

    add the reverse stack class

---
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/ReverseStackWithRecursion.java
@@ -0,0 +1,13 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.stackandqueue;
+
+/**
+ * Using two recursion to reverse the stack
+ * @author cwenao
+ * @version $Id ReverseStackWithRecursion.java, v 0.1 2017-10-05 22:46 cwenao Exp $$
+ */
+public class ReverseStackWithRecursion {
+}
