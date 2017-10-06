424e15a3c8c45794c801f1751e72ec3be4ef06f3
cwenao <cwenao@126.com>
2017-10-06 22:52:53 +0800

    update the reverse stack class

--- data-structure/src/main/java/com/cwenao/trying/stackandqueue/ReverseStackWithRecursion.java
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/ReverseStackWithRecursion.java
@@ -4,10 +4,49 @@
  */
 package com.cwenao.trying.stackandqueue;
 
+import java.util.Stack;
+
 /**
  * Using two recursion to reverse the stack
+ *
  * @author cwenao
  * @version $Id ReverseStackWithRecursion.java, v 0.1 2017-10-05 22:46 cwenao Exp $$
  */
 public class ReverseStackWithRecursion {
+
+    private static Stack<Integer> stack = new Stack<>();
+
+    private static void insertAtBottom(int x) {
+        if (stack.isEmpty()) {
+            stack.push(x);
+        } else {
+            int a = stack.peek();
+            stack.pop();
+            insertAtBottom(x);
+            stack.push(a);
+        }
+    }
+
+    public static void reverseStack() {
+        if (stack.size()>0) {
+            int a = stack.peek();
+            stack.pop();
+            reverseStack();
+            insertAtBottom(a);
+        }
+    }
+
+    public static void main(String[] args) {
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        reverseStack();
+
+        System.out.println(stack);
+
+    }
 }
