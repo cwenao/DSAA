55934f5ca0a034c291fbe3d0ed4c5dde84bedc5c
cwenao <cwenao@126.com>
2017-10-07 22:56:54 +0800

    sort the stack by recursion

---
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/SortStackWithRecursion.java
@@ -0,0 +1,13 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.stackandqueue;
+
+/**
+ * using two recursion to sort the stack, the method like reverse stack
+ * @author cwenao
+ * @version $Id SortStackWithRecursion.java, v 0.1 2017-10-07 23:55 cwenao Exp $$
+ */
+public class SortStackWithRecursion {
+}
