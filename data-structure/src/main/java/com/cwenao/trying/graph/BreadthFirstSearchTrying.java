95e5b892afe1cd8804ca0245bd96fe9a3bd39f1a
cwenao <cwenao@126.com>
2017-10-13 16:00:59 +0800

    add the bfs with queue

---
+++ data-structure/src/main/java/com/cwenao/trying/graph/BreadthFirstSearchTrying.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.graph;
+
+/**
+ * @author cwenao
+ * @version $Id BreadthFirstSearchTrying.java, v 0.1 2017-10-16 15:39 cwenao Exp $$
+ */
+public class BreadthFirstSearchTrying {
+}
