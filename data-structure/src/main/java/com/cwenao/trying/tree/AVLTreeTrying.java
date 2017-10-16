c523074479efb20f38817fd3c4a0aa67f16fce39
cwenao <cwenao@126.com>
2017-10-16 16:48:39 +0800

    add the binary node

---
+++ data-structure/src/main/java/com/cwenao/trying/tree/AVLTreeTrying.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.tree;
+
+/**
+ * @author cwenao
+ * @version $Id AVLTreeTrying.java, v 0.1 2017-10-16 16:30 cwenao Exp $$
+ */
+public class AVLTreeTrying {
+}
---
+++ data-structure/src/main/java/com/cwenao/trying/tree/BinaryNodeTrying.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.tree;
+
+/**
+ * @author cwenao
+ * @version $Id BinaryNodeTrying.java, v 0.1 2017-10-16 16:45 cwenao Exp $$
+ */
+public class BinaryNodeTrying {
+}
