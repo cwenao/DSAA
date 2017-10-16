f4d9df6778f53d008a8e6055192f1d51b798069a
cwenao <cwenao@126.com>
2017-10-16 16:50:42 +0800

    update

--- data-structure/src/main/java/com/cwenao/trying/tree/AVLTreeTrying.java
+++ data-structure/src/main/java/com/cwenao/trying/tree/AVLTreeTrying.java
@@ -6,7 +6,8 @@ package com.cwenao.trying.tree;
 
 /**
  * @author cwenao
- * @version $Id AVLTreeTrying.java, v 0.1 2017-10-16 16:30 cwenao Exp $$
+ * @version $Id AVLTreeTrying.java, v 0.1 2017-10-16 12:30 cwenao Exp $$
  */
-public class AVLTreeTrying {
+public class AVLTreeTrying<T extends Comparable> {
+
 }
--- data-structure/src/main/java/com/cwenao/trying/tree/BinaryNodeTrying.java
+++ data-structure/src/main/java/com/cwenao/trying/tree/BinaryNodeTrying.java
@@ -4,9 +4,47 @@
  */
 package com.cwenao.trying.tree;
 
+import java.io.Serializable;
+
 /**
  * @author cwenao
  * @version $Id BinaryNodeTrying.java, v 0.1 2017-10-16 16:45 cwenao Exp $$
  */
-public class BinaryNodeTrying {
+public class BinaryNodeTrying<T extends Comparable> implements Serializable {
+    private int data;
+    private BinaryNodeTrying<T> leftNode;
+    private BinaryNodeTrying<T> rightNode;
+
+    public BinaryNodeTrying(int data) {
+        this(data, null, null);
+    }
+    public BinaryNodeTrying(int data, BinaryNodeTrying leftNode, BinaryNodeTrying rightNode) {
+        this.data = data;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public BinaryNodeTrying<T> getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(BinaryNodeTrying<T> leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public BinaryNodeTrying<T> getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(BinaryNodeTrying<T> rightNode) {
+        this.rightNode = rightNode;
+    }
 }
