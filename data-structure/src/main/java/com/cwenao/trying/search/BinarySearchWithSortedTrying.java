e0ce90f5faabb8e9aa6151d4aa97f706087554a4
cwenao <cwenao@126.com>
2017-10-19 00:33:30 +0800

    add the binary search with sorted array

---
+++ data-structure/src/main/java/com/cwenao/trying/search/BinarySearchWithSortedTrying.java
@@ -0,0 +1,36 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.search;
+
+/**
+ * @author cwenao
+ * @version $Id BinarySearchWithSortedTrying.java, v 0.1 2017-10-17 13:36 cwenao Exp $$
+ */
+public class BinarySearchWithSortedTrying {
+
+    public static int binarySearchIndex(int[] array, int target, int low, int high) {
+
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (high + low) / 2;
+
+        if (target == array[mid]) {
+            return mid;
+        }
+
+        if (target > array[mid]) {
+            return binarySearchIndex(array, target, low + 1, high);
+        } else {
+            return binarySearchIndex(array, target, low, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] targetArray = {1, 2, 4, 6, 8, 11, 34, 55, 66, 75, 78, 100, 1000, 10000};
+        System.out.println(binarySearchIndex(targetArray,66,0,targetArray.length-1));
+    }
+}
