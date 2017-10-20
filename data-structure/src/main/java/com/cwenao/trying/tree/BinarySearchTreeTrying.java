a721e38cf7cceddaf3a1991e610411070d40c282
cwenao <cwenao@126.com>
2017-10-20 08:26:20 +0800

    add the binary search tree

---
+++ data-structure/src/main/java/com/cwenao/trying/tree/BinarySearchTreeTrying.java
@@ -0,0 +1,20 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.tree;
+
+/**
+ * 树定义
+ * 结点的度：结点拥有的子树的数目。
+ * 叶子：度为零的结点。
+ * 分支结点：度不为零的结点。
+ * 树的度：树中结点的最大的度。
+ *
+ * 层次：根结点的层次为1，其余结点的层次等于该结点的双亲结点的层次加1。
+ * 树的高度：树中结点的最大层次。
+ * @author cwenao
+ * @version $Id BinarySearchTreeTrying.java, v 0.1 2017-10-20 08:44 cwenao Exp $$
+ */
+public class BinarySearchTreeTrying {
+}
