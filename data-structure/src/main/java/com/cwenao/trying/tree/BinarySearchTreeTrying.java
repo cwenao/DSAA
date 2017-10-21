9c17e87fc8b4ecc5d6327777fa723d006e7795a2
cwenao <cwenao@126.com>
2017-10-21 08:40:39 +0800

    update the BSTs

--- data-structure/src/main/java/com/cwenao/trying/tree/BinarySearchTreeTrying.java
+++ data-structure/src/main/java/com/cwenao/trying/tree/BinarySearchTreeTrying.java
@@ -13,8 +13,19 @@ package com.cwenao.trying.tree;
  *
  * 层次：根结点的层次为1，其余结点的层次等于该结点的双亲结点的层次加1。
  * 树的高度：树中结点的最大层次。
+ *
+ * 二叉树性质：
+ * 性质1：二叉树第i层上的结点数目最多为 2{i-1} (i≥1)。
+ * 性质2：深度为k的二叉树至多有2{k}-1个结点(k≥1)。
+ * 性质3：包含n个结点的二叉树的高度至少为log2 (n+1)。
+ * 性质4：在任意一棵二叉树中，若终端结点的个数为n0，度为2的结点数为n2，则n0=n2+1
+ *
+ * @link http://www.cnblogs.com/skywang12345/p/3576328.html
  * @author cwenao
  * @version $Id BinarySearchTreeTrying.java, v 0.1 2017-10-20 08:44 cwenao Exp $$
  */
-public class BinarySearchTreeTrying {
+public class BinarySearchTreeTrying<T extends Comparable> {
+
+
+
 }
