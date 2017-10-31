b48d977f8df4b53df75c716ff041dc7b994c5b1d
cwenao <cwenao@126.com>
2017-10-31 09:22:15 +0800

    add the max top heap

---
+++ data-structure/src/main/java/com/cwenao/trying/tree/MaxTopHeapByArrays.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.tree;
+
+/**
+ * @author cwenao
+ * @version $Id MaxTopHeapByArrays.java, v 0.1 2017-10-31 09:21 cwenao Exp $$
+ */
+public class MaxTopHeapByArrays {
+}
