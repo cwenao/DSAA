189ba81de2857eed5698e96acef5d11dbbf6d35f
cwenao <cwenao@126.com>
2017-11-01 09:25:47 +0800

    update the max top heap

--- data-structure/src/main/java/com/cwenao/trying/tree/MaxTopHeapByArrays.java
+++ data-structure/src/main/java/com/cwenao/trying/tree/MaxTopHeapByArrays.java
@@ -8,5 +8,22 @@ package com.cwenao.trying.tree;
  * @author cwenao
  * @version $Id MaxTopHeapByArrays.java, v 0.1 2017-10-31 09:21 cwenao Exp $$
  */
-public class MaxTopHeapByArrays {
+public class MaxTopHeapByArrays<T extends Comparable> {
+
+    private T[] heaps;
+
+    private int n;
+
+
+
+    private boolean less(int i, int j) {
+        return heaps[i].compareTo(heaps[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        T tmp = heaps[i];
+        heaps[i] = heaps[j];
+        heaps[j] = tmp;
+    }
+
 }
