6185d390ca770544e51416e1a593d31e8d0214ec
cwenao <cwenao@126.com>
2017-11-25 08:49:43 +0800

    add the LRU class

---
+++ data-structure/src/main/java/com/cwenao/trying/stackandqueue/LRUCacheImplementWithQuery.java
@@ -0,0 +1,18 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.trying.stackandqueue;
+
+/**
+ * @author cwenao
+ * @version $Id LRUCacheImplementWithQuery.java, v 0.1 2017-11-25 08:43 cwenao Exp $$
+ * Queue which is implemented using a doubly linked list.
+ * The maximum size of the queue will be equal to the total number of frames available (cache size).
+ * The most recently used pages will be near front end and least recently pages will be near rear end.
+ */
+public class LRUCacheImplementWithQuery {
+
+
+
+}
