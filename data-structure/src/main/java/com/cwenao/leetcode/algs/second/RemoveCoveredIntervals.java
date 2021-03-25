76776ecae0cc806278c062cf33fd5423f68e6966
cwenao <cwenao@126.com>
2021-03-25 22:51:56 +0800

    :construction: 删除重合区域

---
+++ data-structure/src/main/java/com/cwenao/leetcode/algs/second/RemoveCoveredIntervals.java
@@ -0,0 +1,56 @@
+/*
+ * Company
+ * Copyright (C) 2014-2021 All Rights Reserved.
+ */
+package com.cwenao.leetcode.algs.second;
+
+import java.util.Arrays;
+
+/**
+ * TODO : Statement the class description
+ *
+ * @author cwenao
+ * @version $Id RemoveCoveredIntervals.java, v1.0.0 2021-03-25 09:23 cwenao Exp $$
+ */
+public class RemoveCoveredIntervals {
+
+    public int removeCoveredIntervals(int[][] intervals) {
+
+        Arrays.sort(intervals,(a,b) ->{
+            //先按第一个数排序，第一个相等再按第二个数排序
+            if (a[0] == b[0]) {
+                return b[1] - b[0];
+            }
+            return a[0] - b[0];
+        });
+
+        //初始化
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        //覆盖区间数量
+        int num = 0;
+
+        for (int i = 0; i < intervals.length; i++) {
+            int[] tmp = intervals[i];
+
+            //全覆盖
+            if (start <= tmp[0] && end >= tmp[1]) {
+                num++;
+            }
+
+            //合并区间
+            if (end >= tmp[0] && end <= tmp[1]) {
+                end = tmp[1];
+            }
+
+            //非重叠区间，更新区间大小
+            if (end < tmp[0]) {
+                start = tmp[0];
+                end = tmp[1];
+            }
+        }
+
+        return intervals.length - num;
+    }
+
+}
